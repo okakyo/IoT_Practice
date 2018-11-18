d4bb37cbfff3ea39952937958b0c86164131de43
Kyohei Oka <0622okakyo@gmail.com>
2018-11-18 14:16:00 +0900

    '練習中'

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
-from machine import Pin
+from machine import Pin,pwd
 import network,socket,time,urequests
 
+ml_pin=15
+mr_pin=10
+led_1=1
+led_2=2
 
+sensor_1=3
+sensor_2=4
+sensor_3=5
+sensor_4=6
+sensor_5=7
 
-class Connect_net:
+#グローバル上で、ピン設定の役割を入力しておく。
+
+motor_left=Pin(ml_pin,Pin.OUT)
+motor_right=Pin(mr_pin,Pin.OUT)
+led1=Pin(led_1,Pin.OUT)
+led2=Pin(led_2,Pin.OUT)
+
+def motor(left,right):
+    motor_left.value(left)
+    motor_right.value(right)
+    time.sleep(1)
+
+def servo(value):
+    pass    
+
+def micro_wave(value):
+    pass
+
+def red_wave(value):
+    pass
+
+class Send_data:
+
     def __init__(self):
+        self.url=''
+        self.json=''
+
+    def send_data(self):
         pass
     
-    def connect():
-        pass
+    def parse_data(self):
+        pass 
 
-pi=Pin(15,Pin.OUT)
-
-def motor():
-    pass
 
 
 if __name__=='__main__':
-    net=Connect_net()
-    net.connect()
+    pass
+
