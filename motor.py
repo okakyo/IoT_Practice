dc1edb658178fb22806eb01221e99303fa0c63a3
Kyohei Oka <0622okakyo@gmail.com>
2018-11-22 20:11:22 +0900

    '練習中'

--- motor.py
+++ motor.py
@@ -1,5 +1,7 @@
 from machine import Pin,PWM
-import time
+import utime
+import cmath
+
 
 
 pin_left=Pin(15,Pin.OUT)
@@ -8,18 +10,13 @@ pin_servo=Pin(8,Pin.OUT)
 servo=PWM(pin_servo,freq=2000,duty=100)
 
 def motor(left,right):
-
     pin_left.value(left)
     pin_right.value(right)
     time.sleep(1.0)
 
 def servo():
-    servo.pwm()
-    time.sleep(1.0)
+    @pass 
 
-for i in range(3):
-    motor(1,0)
-    motor(0,0)
-    motor(0,1)
-    motor(0,0)
+if __name__=='__main__':
+    pass
 
