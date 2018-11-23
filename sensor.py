eec288ddf498e0758c8f1f19e193bb1fec7ef907
Kyohei Oka <0622okakyo@gmail.com>
2018-11-23 19:49:13 +0900

    '練習中'

---
+++ sensor.py
@@ -0,0 +1,19 @@
+from machine import Pin
+import math
+
+send_pin=Pin(10,Pin.INPUT)
+send_pin=Pin(15,Pin.INPUT)
+read_pin=Pin(3,Pin.OUTPUT)
+read_pin=Pin(4,Pin.OUTPUT)
+read_pin=Pin(5,Pin.OUTPUT)
+
+def set_up():
+    pass
+
+def show_data():
+    pass
+
+if __name__=='__main__':
+    pass
+    
+
