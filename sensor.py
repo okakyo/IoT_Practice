84786b03a889dda69885a80ed30125a396b3d5dd
Kyohei Oka <0622okakyo@gmail.com>
2018-11-25 20:54:18 +0900

    '練習中'

--- sensor.py
+++ sensor.py
@@ -1,16 +1,18 @@
 from machine import Pin
 import math
 
-send_pin=Pin(10,Pin.INPUT)
-send_pin=Pin(15,Pin.INPUT)
-read_pin=Pin(3,Pin.OUTPUT)
-read_pin=Pin(4,Pin.OUTPUT)
-read_pin=Pin(5,Pin.OUTPUT)
 
 def set_up():
-    pass
+    send_pin=Pin(10,Pin.INPUT)
+    send_pin=Pin(15,Pin.INPUT)
+    read_pin=Pin(3,Pin.OUTPUT)
+    read_pin=Pin(4,Pin.OUTPUT)
+    read_pin=Pin(5,Pin.OUTPUT)
 
 def show_data():
+    pass
+
+def loop():
     pass
 
 if __name__=='__main__':
