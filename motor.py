0ead0151b206a5f3fb143f2daddbc62bfa7d9e72
Kyohei Oka <0622okakyo@gmail.com>
2018-11-26 19:25:15 +0900

    '練習中'

--- motor.py
+++ motor.py
@@ -1,22 +1,12 @@
-from machine import Pin,PWM
-import utime
-import cmath
-
-
+from machine import Pin
 
 pin_left=Pin(15,Pin.OUT)
 pin_right=Pin(10,Pin.OUT)
-pin_servo=Pin(8,Pin.OUT)
-servo=PWM(pin_servo,freq=2000,duty=100)
 
 def motor(left,right):
     pin_left.value(left)
     pin_right.value(right)
     time.sleep(1.0)
 
-def servo():
-    @pass 
-
 if __name__=='__main__':
-    pass
-
+    motor(1,0)
