9e0d48e7b83242152646c275f95705995210037d
Kyohei Oka <0622okakyo@gmail.com>
2018-12-15 14:45:43 +0900

    '練習中'

--- sensor.py
+++ sensor.py
@@ -4,43 +4,45 @@ import math,time
 #いかに赤外センサーを使えるようにするかについて記述する必要がある。
 # => クラスにした方が、それぞれのセンサーに対応できる。
 
-def setup():
-    global trig,echo
-    trig=Pin(7,Pin.OUT)
-    echo=Pin(6,Pin.IN)
+class Wave:
+    
+    def __init__(self,p1,p2):
+        self.trig=Pin(p1,Pin.OUT)
+        self.echo=Pin(p2,Pin.IN)
 
-def read():
-    global trig,echo
-    t1=t2=None
-    tick=0.0001
-    trig.value(1)
-    time.sleep(0.00001)
-    trig.value(0)
-    for _ in range(10000):
-        if echo.value():
-            t1=time.ticks_us()
-            break
-        time.sleep(tick)
+    def __read(self):
+        t1=t2=None
+        tick=0.0001
+        self.trig.value(1)
+        time.sleep(0.00001)
+        self.trig.value(0)
+        for _ in range(10000):
+            if self.echo.value():
+                t1=time.ticks_us()
+                break
+            time.sleep(tick)
 
-    if not  t1:
-        raise RuntimeError
+        if not  t1:
+            raise RuntimeError
 
-    for _ in range(10000):
-        if not echo.value():
-            t2=time.ticks_us()
-            break
-        time.sleep(tick)
-    if not t2:
-        raise RuntimeError
+        for _ in range(10000):
+            if not self.echo.value():
+                t2=time.ticks_us()
+                break
+            time.sleep(tick)
+        if not t2:
+            raise RuntimeError
 
-    return float(0.017*(t2-t1))
-        
-def main():
-    setup()
-    while True:
-        try:
-            print(read())
-        except RuntimeError:
-            pass
-        time.sleep(1)
+        return float(0.017*(t2-t1))
+            
+    def main(self):
+        while True:
+            try:
+                print(self.__read())
+            except RuntimeError:
+                pass
+            time.sleep(1)
 
+if __name__=='__main__':
+    wave=Wave(33,25)
+    wave.main()
